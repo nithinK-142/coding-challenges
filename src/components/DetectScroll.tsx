19dae773f53117e05e6b189d5d64130ab75160c9
nithinK-142 <nithin.sagar.359@gmail.com>
2024-04-27 10:15:46 +0530

    update: use useScrollDetection directly in DetectScroll.

--- src/components/DetectScroll.tsx
+++ src/components/DetectScroll.tsx
@@ -1,4 +1,26 @@
-import useScrollDetection from "@/hooks/useScrollDetection";
+import { useState, useRef, useEffect } from "react";
+
+const useScrollDetection = () => {
+  const [scrollDirection, setScrollDirection] = useState("START");
+  const scrollY = useRef(0);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    window.scrollY === 0
+      ? setScrollDirection("START")
+      : scrollY.current > window.scrollY
+      ? setScrollDirection("UP 👆")
+      : setScrollDirection("DOWN 👇");
+
+    scrollY.current = window.scrollY;
+  };
+
+  return scrollDirection;
+};
 
 const DetectScrollComponent = () => {
   const scrollDirection = useScrollDetection();
