2df79d880f857a9fbb456319f37c7d01f2ef7bd4
nithinK-142 <nithin.sagar.359@gmail.com>
2024-05-17 10:42:18 +0530

    update: navbar css.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,12 +20,12 @@ const Navbar: React.FC = () => {
   ];
   return (
     <nav className="flex justify-center p-3 mb-10 bg-gray-600 sm:px-6 lg:px-8">
-      <div className="flex space-x-4">
+      <div className="flex flex-wrap space-x-4">
         {navs.map(({ label, link }) => (
           <Link
             key={link}
             to={link}
-            className={`text-gray-300 rounded-md px-3 py-2 text-sm font-medium hover:text-white ${
+            className={`text-gray-300 rounded-md px-3 py-2 text-sm font-medium hover:text-white mb-2 ${
               pathname === link ? "bg-gray-900" : "hover:bg-gray-700"
             }`}
           >
